8ed6a769f9c73b17ec695625599c38fd92f683ad
Dimitar Yanev <mitko.yanev21@gmail.com>
2017-06-01 23:26:44 +0300

    modifield folder

---
+++ Homeworks - Mitko/Data-Types-and-Variables-Exercises - Mitko/02. Practice Floating Points/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Practice_Floating_Points
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal pi = 3.141592653589793238m;
+            Console.WriteLine(pi);
+            decimal num = 1.60217657m;
+            Console.WriteLine(num);
+            decimal num2 = 7.8184261974584555216535342341m;
+            Console.WriteLine(num2);
+        }
+    }
+}
