cf87a0daadf2b4e9fc2c885ed895f64ebcfa569a
Dimitar Yanev <mitko.yanev21@gmail.com>
2017-06-01 14:52:08 +0300

    adding Data-Types-and-Variables-Exercises.sln

---
+++ TestProgram.30.05.2017/TestProgram.30.05.2017/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProgram._30._05._2017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
