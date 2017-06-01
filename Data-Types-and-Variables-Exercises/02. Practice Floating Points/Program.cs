0b59717200c27b608a7adfd4d0fd94bc478e77b4
Dimitar Yanev <mitko.yanev21@gmail.com>
2017-06-01 15:18:12 +0300

    //

---
+++ Data-Types-and-Variables-Exercises/02. Practice Floating Points/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Practice_Floating_Points
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.141592653589793238;
+            Console.WriteLine(pi);
+        }
+    }
+}
---
+++ Data-Types-and-Variables-Exercises/Data-Types-and-Variables-Exercises/01. Practice Integers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Types_and_Variables_Exercises
+{
+    class 01. Practice Integers
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
