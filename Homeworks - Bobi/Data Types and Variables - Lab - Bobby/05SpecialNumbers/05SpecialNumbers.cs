3062a690beb2dcca779633b1b2cac1e056d9c7e2
BGeorgiev92 <b_long@abv.bg>
2017-06-02 12:38:53 +0300

    adding all programs

---
+++ Homeworks - Bobi/Data Types and Variables - Lab - Bobby/05SpecialNumbers/05SpecialNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05SpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int num = 1; num <= n; num++)
+            {
+                int sumOfDigits = 0;
+                int lastDigit = num;
+                while (lastDigit > 0)
+                {
+                    sumOfDigits += lastDigit % 10;
+                    lastDigit = lastDigit / 10;
+                }
+                if (sumOfDigits == 5 || sumOfDigits == 7 || sumOfDigits == 11)
+                {
+                    Console.WriteLine($"{num} -> True");
+                }
+                else
+                {
+                    Console.WriteLine($"{num} -> False");
+                }
+            }
+        }
+    }
+}
---
+++ Homeworks - Bobi/Data Types and Variables - Lab - Bobby/09RefactorSpecialNumbers/09RefactorSpecialNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09RefactorSpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            for (int num = 1; num <= number; num++)
+            {
+                int sum = 0;
+                int digit = num;
+                while (num > 0)
+                {
+                    sum += num % 10;
+                    num = num / 10;
+                }
+                bool magicNumbers = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine($"{digit} -> {magicNumbers}");
+                num = digit;
+            }
+
+        }
+    }
+}
